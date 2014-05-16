5fc6ef6ba067e3aeed4ab7d098df0f1a9f14b832
Patrick Cable <cable@ll.mit.edu>
2014-05-16 15:38:28 -0600

    wired up interface for session creation and container creator interaction

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,32 @@
+require 'httparty'
+require 'json'
+
 class SessionsController < ApplicationController
   def new
+    @user = current_user
+    @session = Session.new
+  end
+
+  def create
+    @session = Session.new(session_params)
+    @session.user_id = current_user.id
+    # Start the HTTParty
+    @result = HTTParty.post("http://localhost:5000/new", 
+                  :body => { :DbKeyId => @session.key_id,
+                             :DbUserId => @session.user_id 
+                           }.to_json, 
+                  :headers => { 'Content-Type' => 'application/json' } )
+    redirect_to root_path
   end
 
   def index
   end
 
   def show
+  end
+
+  private
+  def session_params
+    params.require(:session).permit(:key_id)
   end
 end
