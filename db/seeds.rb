ee68cbfeeb2510ea4516e42b218329631b2991b7
Patrick Cable <cable@ll.mit.edu>
2014-05-30 12:56:52 -0400

    set up default admin account

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(login: 'admin', password: 'admin', use_yubikey: false)
