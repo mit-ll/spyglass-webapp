32cacbea277dca0a6c0acdd0f63190a32a9020e4
Patrick Cable <cable@ll.mit.edu>
2014-05-30 13:00:34 -0400

    inital user needs to be admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(login: 'admin', password: 'admin', use_yubikey: false)
+User.create!(login: 'admin', password: 'admin', use_yubikey: false, roles_mask: 3)
